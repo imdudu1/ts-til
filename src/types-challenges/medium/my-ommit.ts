bda7cf20b5729f7def5083458fd4db0c6a9688b8
Byeongju, Shin <cd80@kakao.com>
2022-02-04 19:03:27 +0900

    feat(): Types challenges medium 3문제 풀이 추가

---
+++ src/types-challenges/medium/my-ommit.ts
@@ -0,0 +1,23 @@
+type MyOmit<T, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
+
+/*
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>
+]
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+  completed: boolean
+}
+
+interface Expected2 {
+  title: string
+}
+*/
