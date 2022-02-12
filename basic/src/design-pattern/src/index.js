0bed5113899e814048672e33085491f6f3ef8856
Byeongju, Shin <cd80@kakao.com>
2022-02-12 19:59:28 +0900

    style: 들여쓰기 수정

--- basic/src/design-pattern/src/index.js
+++ basic/src/design-pattern/src/index.js
@@ -1,51 +1,51 @@
 function Human(name, age, address) {
-    // Private variables
-    const _address = address;
+  // Private variables
+  const _address = address;
 
-    // Public variables
-    this.name = name;
-    this.age = age;
+  // Public variables
+  this.name = name;
+  this.age = age;
 
-    // Public methods
-    this.profile = function () {
-        // Accessing private member variables
-        return `${nameAndAge()}, ${_address}`;
-    }
+  // Public methods
+  this.profile = function () {
+    // Accessing private member variables
+    return `${nameAndAge()}, ${_address}`;
+  }
 
-    // Private methods
-    const nameAndAge = () => {
-        return `${this.name} (${this.age})`;
-    }
+  // Private methods
+  const nameAndAge = () => {
+    return `${this.name} (${this.age})`;
+  }
 
-    this.work = function () {
-        throw new Error('you have to build your own \'work\' method');
-    }
+  this.work = function () {
+    throw new Error('you have to build your own \'work\' method');
+  }
 }
 
 // Public methods
 Human.prototype.sayHi = function () {
-    console.log(`${this.name} (${this.age}) : Hi!`)
+  console.log(`${this.name} (${this.age}) : Hi!`)
 }
 
 // Static methods
 Human.of = function (name, age, address) {
-    return new Human(name, age, address);
+  return new Human(name, age, address);
 }
 
 function Programmer(name, age, address, lang) {
-    Human.apply(this, arguments);
-    this.lang = lang;
+  Human.apply(this, arguments);
+  this.lang = lang;
 
-    let coffeeCount = 0;
-    this.drinkCoffee = function () {
-        console.log(++coffeeCount);
-    }
+  let coffeeCount = 0;
+  this.drinkCoffee = function () {
+    console.log(++coffeeCount);
+  }
 
-    this.work = function () {
-        console.log(`${this.name} is coding a ${this.lang} program!`)
-    }
+  this.work = function () {
+    console.log(`${this.name} is coding a ${this.lang} program!`)
+  }
 
-    return Object.seal(this);
+  return Object.seal(this);
 }
 Programmer.prototype = Object.create(Human.prototype);
 Programmer.prototype.constructor = Programmer;
