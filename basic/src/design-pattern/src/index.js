25db48bbaeb2ac4d7356ebfc7b35b80a1362e143
Byeongju, Shin <cd80@kakao.com>
2022-02-12 16:51:40 +0900

    refactor: example4

--- basic/src/design-pattern/src/index.js
+++ basic/src/design-pattern/src/index.js
@@ -1,17 +1,40 @@
-function Human(name, age) {
+function Human(name, age, address) {
+    // Private variables
+    const _address = address;
+
+    // Public variables
     this.name = name;
     this.age = age;
+
+    // Public methods
+    this.profile = function () {
+        // Accessing private member variables
+        return `${nameAndAge()}, ${_address}`;
+    }
+
+    // Private methods
+    const nameAndAge = () => {
+        return `${this.name} (${this.age})`;
+    }
 }
 
+// Public methods
 Human.prototype.sayHi = function () {
     console.log(`${this.name} (${this.age}) : Hi!`)
 }
 
-const obj = new Human('Byeongju, Shin', 28);
+// Static methods
+Human.of = function (name, age, address) {
+    return new Human(name, age, address);
+}
 
-const obj2 = new Human('Bitcake0', 28);
+const obj = new Human('Byeongju, Shin', 28, 'Github');
+
+const obj2 = Human.of('Bitcake0', 28, 'Github aid95');
 
 obj.sayHi();
 obj2.sayHi();
 
 console.log(obj.sayHi === obj2.sayHi); // true
+
+console.log(obj.profile());
