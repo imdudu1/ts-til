292bd08f41bf7b0e904e76e2554ab2b8b93ffc27
Byeongju, Shin <cd80@kakao.com>
2022-03-19 15:36:57 +0900

    feat: fxjs 학습

--- fx/__test__/fxjs-strict.test.js
+++ fx/__test__/fxjs-strict.test.js
@@ -11,7 +11,27 @@ import {
   delay,
   difference,
   differenceBy,
+  differenceWith,
+  divide,
+  drop,
+  dropRight,
+  dropUntil,
+  entries,
   go,
+  identity,
+  mapEntries,
+  max,
+  mean,
+  meanBy,
+  min,
+  omit,
+  omitBy,
+  partition,
+  range,
+  reduce,
+  remove,
+  repeat,
+  sel,
 } from "fxjs";
 
 describe("Strict Tests", function () {
@@ -120,5 +140,86 @@ describe("Strict Tests", function () {
     const [result] = differenceBy(fn, data, oldData);
 
     expect(result.name).toBe("anna");
+  });
+
+  test("differenceWith", function () {
+    const cmp = (x, y) => x.a === y.a;
+    const l1 = [{ a: 1 }, { a: 2 }, { a: 3 }];
+    const l2 = [{ a: 1 }, { a: 3 }];
+
+    expect(differenceWith(cmp, l1, l2)).toStrictEqual([{ a: 2 }]);
+  });
+
+  test("divide", function () {
+    expect(divide(4, 2)).toBe(2);
+  });
+
+  test("drop", function () {
+    expect(drop(1, drop(1, [1, 2, 3, 4]))).toStrictEqual([3, 4]);
+  });
+
+  test("dropUntil", function () {
+    expect(dropUntil((v) => 1 < v, [1, 2, 3, 4, 5])).toStrictEqual([3, 4, 5]);
+  });
+
+  test("mapEntries", function () {
+    expect(mapEntries((v) => v + 10, entries({ a: 1, b: 2 }))).toStrictEqual([
+      ["a", 11],
+      ["b", 12],
+    ]);
+  });
+
+  test("max", function () {
+    expect(max([1, 3, 7, 4, 2])).toBe(7);
+  });
+
+  test("mean", function () {
+    expect(mean([1, 2, 3, 4, 5, 6])).toBe(3.5);
+  });
+
+  test("meanBy", function () {
+    expect(meanBy(identity, [1, 2, 3, 4, 5, 6])).toBe(3.5);
+    expect(meanBy(sel("age"), [{ age: 10 }, { age: 20 }])).toBe(15);
+  });
+
+  test("min", function () {
+    expect(min([10, 1000, 3, 1, 3, 4, 5])).toBe(1);
+  });
+
+  test("omit", function () {
+    expect(omit(["a", "c"], { a: 1, b: 2, c: 3, d: 4 })).toStrictEqual({
+      b: 2,
+      d: 4,
+    });
+  });
+
+  test("omitBy", function () {
+    expect(
+      omitBy(([_, v]) => v % 2 === 0, { a: 1, b: 2, c: 3, d: 4 })
+    ).toStrictEqual({ a: 1, c: 3 });
+  });
+
+  test("partition", function () {
+    const p = partition((v) => v % 2, [1, 2, 3, 4, 5]);
+    expect(p[0]).toStrictEqual([1, 3, 5]);
+    expect(p[1]).toStrictEqual([2, 4]);
+  });
+
+  test("range", function () {
+    expect(range(0, 20, 5)).toStrictEqual([0, 5, 10, 15]);
+  });
+
+  test("reduce", function () {
+    const add = (a, b) => a + b;
+    expect(reduce(add, [1, 2, 3, 4])).toBe(10);
+  });
+
+  test("remove", function () {
+    expect(remove(0, [1, 2, 3, 4, 5])).toStrictEqual([2, 3, 4, 5]);
+  });
+
+  test("repeat", function () {
+    const add = (a, b) => a + b;
+    expect(go(repeat(1, 10), reduce(add))).toBe(10);
   });
 });
