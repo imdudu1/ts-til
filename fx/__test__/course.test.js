a61fd662d957cebc2162e0aafdb65e8653293087
Byeongju, Shin <cd80@kakao.com>
2022-04-06 18:58:10 +0900

    feat: 강의 조회 구현

---
+++ fx/__test__/course.test.js
@@ -0,0 +1,79 @@
+import { FxSQL_DEBUG, PostgreSQL } from "fxsql";
+import { dbEnv } from "../src/config/db.init";
+import { go, take1, mapL } from "fxjs";
+
+describe("Course Tests", () => {
+  let p;
+
+  beforeAll(function () {
+    FxSQL_DEBUG.LOG = true;
+
+    const { CONNECT } = PostgreSQL;
+    p = CONNECT(dbEnv);
+  });
+
+  afterAll(function () {
+    p.END();
+  });
+
+  test("특정 강의명을 가진 강의를 조회한다.", async () => {
+    // given
+    const title = "Aut modi voluptates incidunt aut ut officia adipisci.";
+
+    // when
+    const [result] = await go(
+      p.QUERY`SELECT ${p.CL("id")} FROM ${p.TB("courses")} WHERE ${p.EQ({
+        title,
+      })}`,
+      mapL(({ id }) => id),
+      take1
+    );
+
+    // then
+    expect(result).toBe(1);
+  });
+
+  describe("강의를 조회한다.", () => {
+    test("열린 강의를 조회한다.", async () => {
+      // given
+
+      // when
+      const result = await go(
+        p.QUERY`SELECT ${p.CL("id")} FROM ${p.TB("courses")} WHERE ${p.EQ({
+          status: "OPEN",
+        })}`
+      );
+
+      // then
+      expect(result.length).toBe(5);
+    });
+
+    test("열린 강의들의 수강생 수를 조회한다.", async () => {
+      // given
+      /*
+select *
+from
+  (select * from courses where status = 'OPEN' and deleted_at is null) c
+  inner join instructors i on c.instructor_id = i.id
+  inner join categories ct on c.category_id = ct.id
+  left join (
+    select course_id, count(course_id) as total_students
+    from
+      subscribe_courses sc
+      inner join users u on sc.user_id = u.id and u.deleted_at is null
+    group by course_id) ts on c.id = ts.course_id and c.id = ts.course_id
+;
+      */
+
+      // when
+      const result = await p.QUERY`
+      select * 
+      from 
+        (select * from ${p.TB("courses")} where ${p.EQ({ status: "OPEN" })}) c
+        inner join ${p.TB("instructors")} i on c.instructor_id = i.id`;
+
+      // then
+      console.log(result);
+    });
+  });
+});
